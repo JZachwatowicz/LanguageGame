8b2234e1655a81ed2cabde1a5f3ec8db345e73d3
JZachwtowicz <juliazachwatowicz@gmail.com>
2024-01-09 12:54:51 +0100

    Initial Commit

---
+++ LanguageGame/LanguageGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageGameApp.swift
+//  LanguageGame
+//
+//  Created by student on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LanguageGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
